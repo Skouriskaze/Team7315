a30ff0694e27698c03b33104e82d634965fc2fd9
skouriskaze <skouriskaze@gmail.com>
2017-11-21 00:00:51 -0500

    Added aid screen

---
+++ app/src/main/java/a7315/jd/a7315/ActivityAid.java
@@ -0,0 +1,13 @@
+package a7315.jd.a7315;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActivityAid extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aid);
+    }
+}
--- app/src/main/java/a7315/jd/a7315/ActivityRegister.java
+++ app/src/main/java/a7315/jd/a7315/ActivityRegister.java
@@ -2,37 +2,32 @@ package a7315.jd.a7315;
 
 import android.content.Context;
 import android.content.Intent;
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-/**
- * Created by Jesse on 11/16/2017.
- */
-
-public class ActivityLogin extends AppCompatActivity {
+public class ActivityRegister extends AppCompatActivity {
 
     private EditText mUsername;
     private EditText mPassword;
+    private EditText mPasswordConfirmation;
     private Button mLogin;
     private Button mRegister;
     final private Context mContext = this;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-
+        setContentView(R.layout.activity_register);
 
         mUsername = findViewById(R.id.etUsername);
         mPassword = findViewById(R.id.etPassword);
         mLogin = findViewById(R.id.btnLogin);
         mRegister = findViewById(R.id.btnRegister);
 
-        mLogin.setOnClickListener(new View.OnClickListener() {
+        mRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(mContext, ActivityHome.class);
@@ -40,10 +35,10 @@ public class ActivityLogin extends AppCompatActivity {
             }
         });
 
-        mRegister.setOnClickListener(new View.OnClickListener() {
+        mLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(mContext, ActivityRegister.class);
+                Intent intent = new Intent(mContext, ActivityLogin.class);
                 startActivity(intent);
             }
         });
