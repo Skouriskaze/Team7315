b7d1a20464a9be2d1bf9da2106e49ccd7ad28721
Timothy Lai <timothyslai@gmail.com>
2018-02-09 16:52:22 -0500

    Revert "Merge remote-tracking branch 'origin/master'"
    This reverts commit dd20115c1463dabd6e523d7858fb702c0590b58f, reversing
    changes made to efd2f0ae28660f40c584317a34a62f7670ca9b52.

--- app/src/main/java/a7315/jd/a7315/Activities/ActivityLogin.java
+++ app/src/main/java/a7315/jd/a7315/Activities/ActivityLogin.java
@@ -4,12 +4,9 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.KeyEvent;
 import android.view.View;
-import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 
 import a7315.jd.a7315.Contracts.ContractLogin;
 import a7315.jd.a7315.Presenters.PresenterLogin;
@@ -17,7 +14,6 @@ import a7315.jd.a7315.R;
 
 /**
  * Created by Jesse on 11/16/2017.
- * Handles the Login Screen
  */
 
 public class ActivityLogin extends AppCompatActivity implements ContractLogin.View {
@@ -34,7 +30,6 @@ public class ActivityLogin extends AppCompatActivity implements ContractLogin.Vi
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        setTitle("Login");
 
         presenter = new PresenterLogin(this);
 
@@ -50,17 +45,6 @@ public class ActivityLogin extends AppCompatActivity implements ContractLogin.Vi
                 String szUsername = mUsername.getText().toString();
                 String szPassword = mPassword.getText().toString();
                 presenter.login(szUsername, szPassword);
-            }
-        });
-
-        mPassword.setOnEditorActionListener(new EditText.OnEditorActionListener() {
-            @Override
-            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-                if (actionId == EditorInfo.IME_ACTION_DONE) {
-                    mLogin.performClick();
-                    return true;
-                }
-                return false;
             }
         });
 
